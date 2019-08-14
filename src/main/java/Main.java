6aa6b99d1ca3b90408e6295ffb768394ba9ff06a
Roman Bulinski <aboandrus@gmail.com>
2019-08-14 09:26:32 +0200

    Implemented 1,5,9 case

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,30 @@
+import java.util.*;
+
 public class Main {
 
-    public String solution(int n) {
-        return "";
+    public String solution( int n) {
+
+        Map<Integer, List<String>> indexer = new HashMap<>();
+
+        indexer.put(1, Arrays.asList("I", "X", "C"));
+        indexer.put(5, Arrays.asList("V", "L", "D"));
+        indexer.put(9, Arrays.asList("IX", "XC", "CM"));
+
+        String result = "";
+        String num = String.valueOf(n);
+        String[] stringDigits = num.split("");
+
+        Collections.reverse(Arrays.asList(stringDigits));
+
+        for (int i = 0; i < stringDigits.length; i++) {
+            int tempInt = Integer.valueOf( stringDigits[i] );
+            if(tempInt!=0){
+                result = indexer.get(tempInt).get(i) + result;
+            }
+        }
+
+        return result;
+
     }
 
 
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,11 +6,26 @@ public class MainTest {
 
     private Main conversion = new Main();
 
+//    @Test
+//    public void shouldConvertToRoman() {
+//        assertEquals("solution(1) should equal to I", "I", conversion.solution(1));
+//        assertEquals("solution(4) should equal to IV", "IV", conversion.solution(4));
+//        assertEquals("solution(6) should equal to VI", "VI", conversion.solution(6));
+//    }
+
     @Test
-    public void shouldConvertToRoman() {
-        assertEquals("solution(1) should equal to I", "I", conversion.solution(1));
-        assertEquals("solution(4) should equal to IV", "IV", conversion.solution(4));
-        assertEquals("solution(6) should equal to VI", "VI", conversion.solution(6));
+    public void test10(){
+        assertEquals("X",conversion.solution(10));
+    }
+
+    @Test
+    public void test111(){
+        assertEquals("CXI",conversion.solution(111));
+    }
+
+    @Test
+    public void test591(){
+        assertEquals("DXCI",conversion.solution(591));
     }
 
 
